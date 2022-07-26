c33b15c3677d73887fdb53023642b39b7a1ea93d
Pedro Santos <pjjesus@sefaz.al.gov.br>
2022-07-26 14:28:18 -0300

    Make the Money class abstract, move the times declaration to it as an abstract method and create factory method to the Money subclasses

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,5 +1,14 @@
-public class Money {
+public abstract class Money {
     protected int amount;
+
+    abstract public Money times(int multiplier);
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
 
     @Override
     public boolean equals(Object obj) {
--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -5,30 +5,30 @@ import static org.junit.Assert.*;
 public class MoneyTest {
     @Test
     public void testDollarMultiplication() {
-        Dollar five = new Dollar(5);
+        Money five = Money.dollar(5);
 
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
     // Naive implementation:
     // TODO: add tests comparing to a null and comparing to a different type;
     public void testEquality() {
-        assertEquals(new Dollar(5), new Dollar(5));
-        assertNotEquals(new Dollar(5), new Dollar(6));
+        assertEquals(Money.dollar(5), Money.dollar(5));
+        assertNotEquals(Money.dollar(5), Money.dollar(6));
 
-        assertEquals(new Franc(5), new Franc(5));
-        assertNotEquals(new Franc(5), new Franc(6));
+        assertEquals(Money.franc(5), Money.franc(5));
+        assertNotEquals(Money.franc(5), Money.franc(6));
 
-        assertFalse(new Dollar(5).equals(new Franc(5)));
+        assertFalse(Money.dollar(5).equals(Money.franc(5)));
     }
 
     @Test
     public void testFrancMultiplication() {
-        Franc five = new Franc(5);
+        Money five = Money.franc(5);
 
-        assertEquals(new Franc(10), five.times(2));
-        assertEquals(new Franc(15), five.times(3));
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
     }
 }
