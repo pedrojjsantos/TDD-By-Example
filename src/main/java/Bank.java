b59fc5b6039acff655bebe943d2ed89d4311db5c
Pedro Santos <pjjesus@sefaz.al.gov.br>
2022-07-27 14:52:18 -0300

    Created a way to reduce a 2 Franc to a Dollar and Updated the reduce method in the interface to receive a bank that will handle the conversion rates

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,5 +1,9 @@
 public class Bank {
     public Money reduce(Expression src, String to) {
-        return src.reduce(to);
+        return src.reduce(this, to);
+    }
+
+    public int rate(String from, String to) {
+        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
     }
 }
--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -29,8 +29,9 @@ public class Money implements Expression {
         return currency;
     }
 
-    public Money reduce(String to) {
-        return this;
+    public Money reduce(Bank bank, String to) {
+        int rate = bank.rate(currency, to);
+        return new Money(amount / rate, to);
     }
 
     public Money times(int multiplier) {
