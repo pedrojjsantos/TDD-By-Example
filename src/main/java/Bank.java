eeabf59b5c3a9437046325771349bd4533bfcf7d
Pedro Santos <pjjesus@sefaz.al.gov.br>
2022-07-28 12:40:57 -0300

    Created a hashmap in Bank to store the rates used to convert currencies

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,9 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank {
+    private Map<Pair, Integer> rates = new HashMap<>();
+
     public Money reduce(Expression src, String to) {
         return src.reduce(this, to);
     }
 
     public int rate(String from, String to) {
-        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+        if (from.equals(to)) return 1;
+        return rates.get(new Pair(from, to));
+    }
+
+    public void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), rate);
+    }
+
+    private class Pair {
+        String from;
+        String to;
+
+        Pair(String from, String to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Pair that = (Pair) obj;
+            return this.from.equals(that.from) && this.to.equals(that.to);
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
     }
 }
--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -64,8 +64,13 @@ public class MoneyTest {
     @Test
     public void testReduceMoneyWithDifferentCurrency() {
         Bank bank = new Bank();
-//        bank.addRate("CHF", "USD", 2);
+        bank.addRate("CHF", "USD", 2);
         Money result = bank.reduce(Money.franc(2), "USD");
         assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
+    public void testIdentityRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
     }
 }
