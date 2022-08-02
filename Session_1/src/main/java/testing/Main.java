80b3e8857d2b0a5a3797bbaa6791fa6d4b53c6a5
Pedro Santos <pjjesus@sefaz.al.gov.br>
2022-08-02 12:19:07 -0300

    deleted the main class since it wasn't used anymore

--- Session_1/src/main/java/testing/Main.java
+++
@@ -1,8 +0,0 @@
-package testing;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        new TestCaseTest("testRunning").run();
-        new TestCaseTest("testSetUp").run();
-    }
-}
